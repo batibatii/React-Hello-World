7953f0e5bc568ee0bdea391d5fb0c0080c914e19
batikan <nbatikansevil@gmail.com>
2025-08-27 17:52:16 +0300

    Add hide and show functionality to button Add props Add use client in order to mark my some components as client component

--- src/app/button.js
+++ src/app/button.js
@@ -1,7 +1,12 @@
-export default function Button() {
+"use client";
+
+export default function Button({ showCards, onToggle }) {
   return (
     <>
-      <button className="bg-gray-700 text-gray-200 px-8 py-4 rounded-xl border border-gray-600/30 shadow-[0_4px_8px_rgba(0,0,0,0.3)] hover:bg-gray-600 hover:text-gray-100 hover:shadow-[0_2px_4px_rgba(0,0,0,0.3)] active:shadow-[inset_0_2px_4px_rgba(0,0,0,0.4)] active:translate-y-[2px] transition-all duration-150 cursor-pointer tracking-wide">Click Me!</button>
+      <button onClick={onToggle}
+        className="bg-gray-700 text-gray-200 px-8 py-4 rounded-xl border border-gray-600/30 shadow-[0_4px_8px_rgba(0,0,0,0.3)] hover:bg-gray-600 hover:text-gray-100 hover:shadow-[0_2px_4px_rgba(0,0,0,0.3)] active:shadow-[inset_0_2px_4px_rgba(0,0,0,0.4)] active:translate-y-[2px] transition-all duration-150 cursor-pointer tracking-wide">
+        {showCards ? "Hide Cards!" : "Show Cards!"}
+      </button>
     </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,36 @@
+"use client";
+
 import Cards from "./cards.js"
 import Button from "./button.js"
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [showCards, setShowCards] = useState(true);
+
+  const toggleCards = () => {
+    setShowCards(!showCards);
+  };
+
   return (
     <main className="flex flex-col items-center gap-24 mt-20 bg-gray-800 ">
       <div className="bg-gray-900 w-48 h-16 flex items-center justify-center rounded-xl shadow-inner shadow-gray-950/50 border border-gray-950/30">
         <h1 className="text-gray-200 tracking-wider">FIRST HEADING</h1>
       </div>
-      <div className="grid grid-cols-4 gap-10"><Cards></Cards><Cards></Cards><Cards></Cards><Cards></Cards>
-        <Cards></Cards><Cards></Cards><Cards></Cards><Cards></Cards>
-      </div>
-      <Button></Button>
+      {showCards && (
+        <div className="grid grid-cols-4 gap-10">
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+        </div>
+      )}
+
+      <Button showCards={showCards} onToggle={toggleCards}></Button>
     </main>
   );
 }

77765e993045f856e235ca97255518a589afae22
batikan <nbatikansevil@gmail.com>
2025-08-27 13:03:24 +0300

    Add Button and Cards components; update layout and styling in Home page

---
+++ src/app/cards.js
@@ -0,0 +1,10 @@
+export default function Cards() {
+  return (
+    <div className="flex gap-12">
+      <div className="bg-gray-700 w-84 h-88 text-center p-12 pt-9 rounded-2xl shadow-[inset_0_0_8px_rgba(0,0,0,0.4)] border border-gray-600/30 text-gray-200">
+        <h2 className="text-2xl mb-4 tracking-tight">What is Lorem Ipsum?</h2>
+        <p className="leading-relaxed"><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+      </div>
+    </div>
+  )
+}
